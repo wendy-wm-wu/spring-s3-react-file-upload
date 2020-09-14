bbf83435b01a95deccdfff547a99cc361441f6f3
Wendy Wu <wendywu@gitlab.com>
2020-09-14 15:14:55 -0700

    Remove sensitive data

--- aws-image-upload/src/main/java/com/amigoscode/awsimageupload/config/AmazonConfig.java
+++ aws-image-upload/src/main/java/com/amigoscode/awsimageupload/config/AmazonConfig.java
@@ -14,8 +14,8 @@ public class AmazonConfig {
     @Bean
     public AmazonS3 s3() {
         AWSCredentials awsCredentials = new BasicAWSCredentials(
-                "AKIAJNSEIUGBS4FTQOQQ",
-                "mjDGCg3s+N+nrzCP9eKR166s2qERAtaiCSQPS07u"
+                "",
+                ""
         );
 
         return AmazonS3ClientBuilder
